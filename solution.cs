4d4d440d4993f8631b530656bcff0d6893a83202
Kooreis <kooreis.official@gmail.com>
2024-02-16 05:36:15 -0600

    In this update, a call to the MinEditToPalindrome function is added to the Main method. The result of this function is printed to the console.

--- solution.cs
+++ solution.cs
@@ -6,5 +6,6 @@ class Program
     {
         Console.Write("Enter a number: ");
         string number = Console.ReadLine();
+        Console.WriteLine("Minimum edit distance to convert the number to a palindrome: " + MinEditToPalindrome(number));
     }
 }
